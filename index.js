484779819210008651cb0993e050a5d825d3631c
Hariom <hariomkumar01868@gmail.com>
2023-04-02 21:45:13 +0530

    New features added

---
+++ index.js
@@ -0,0 +1,25 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-content");
+
+function opentab(tabname) { 
+    for(tablink of tablinks){
+        tablink.classList.remove('active-link');
+    }   
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove('active-tab');
+    }  
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+function openmenu(){
+    let menu = document.getElementById("sidemenu");
+    menu.style.right="0";
+}
+function closemenu(){
+    let menu = document.getElementById("sidemenu");
+    menu.style.right="-200px";
+}
+
+
