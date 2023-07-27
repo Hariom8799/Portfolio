178bc331eade6ae8be3ae6c0bcbffd08c6d8afc9
Hariom8799 <hariomkumar01868@gmail.com>
2023-07-27 16:21:07 +0530

    Some bugs fixed

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let tablinks = document.getElementsByClassName("tab-links");
 let tabcontents = document.getElementsByClassName("tab-content");
 
 function opentab(tabname) { 
-    for(tablink of tablinks){
+    for(let tablink of tablinks){
         tablink.classList.remove('active-link');
     }   
-    for(tabcontent of tabcontents){
+    for(let tabcontent of tabcontents){
         tabcontent.classList.remove('active-tab');
     }  
     event.currentTarget.classList.add("active-link")
